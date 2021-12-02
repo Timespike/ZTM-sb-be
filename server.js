3b9989da65e1f50822590c6f48a3e79b2c2cd0dc
Timespike <50344633+Timespike@users.noreply.github.com>
2021-12-01 20:24:05 -0600

    setting up database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./Controllers/image.js');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
+        host : 'postgresql-parallel-43582',
         user : 'postgres',
         password : 'test',
         database : 'smart-brain'
